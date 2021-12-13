664bcfa4bd5105ce3535af5e73097c3d60d9316f
Shashi Bawa <shashibawa104@gmail.com>
2021-12-13 02:57:36 +0000

    Updated Conn object

---
+++ play.js
@@ -0,0 +1,23 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "10.0.2.15", 
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on("connect", (data) => {
+   console.log("connected");
+   setTimeout(()=>{
+    console.log("you ded cuz you idled");
+   }, 5000);
+  })
+
+  return conn;
+};
+
+console.log("Connecting ...");
+console.log(connect());
