e7d3cc48682c1f10c1000f9f950a7e0d0ff254da
Shashi Bawa <shashibawa104@gmail.com>
2021-12-13 05:45:31 +0000

    Imported client.js

--- play.js
+++ play.js
@@ -1,23 +1,4 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "10.0.2.15", 
-    port: 50541,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on("connect", (data) => {
-   console.log("connected");
-   setTimeout(()=>{
-    console.log("you ded cuz you idled");
-   }, 5000);
-  })
-
-  return conn;
-};
-
+// const net = require("net");
+const connect = require("./client");
 console.log("Connecting ...");
-console.log(connect());
+connect();
