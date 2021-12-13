ecb9746ce67c340f957dda2f516b14f57a712ab0
Shashi Bawa <shashibawa104@gmail.com>
2021-12-13 06:36:20 +0000

    Added wasd movements

--- input.js
+++ input.js
@@ -1,14 +1,25 @@
-const setupInput = function () {
+let connection;
+const setupInput = function (conn) {
+    connection = conn;
     const stdin = process.stdin;
     stdin.setRawMode(true);
     stdin.setEncoding("utf8");
     stdin.resume();
-    setupInput.on("data", (key)=>handleUserInput(key))
+    stdin.on("data", (key)=>handleUserInput(key))
     return stdin;
   };
-  const handleUserInput = function () {
+  const handleUserInput = function (key) {
     if (key === '\u0003') {
-      process.exit();
+      process.exit();     
+   }
+  if(key === 'w'){
+    connection.write('Move: up')
+  }else if(key === 's'){
+    connection.write('Move: down')
+  }else if(key === 'a'){
+    connection.write('Move: left')
+  }else if(key === 'd'){
+    connection.write('Move: right')
   }
   }
   module.exports = setupInput;
